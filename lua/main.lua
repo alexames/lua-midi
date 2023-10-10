70302f66fe79aadead3dce1d612e7c4afb776061
Alexander Ames <Alexander.Ames@gmail.com>
2023-10-09 21:53:38 -0700

    Mary had a little lamb

--- lua/main.lua
+++ lua/main.lua
@@ -3,6 +3,7 @@ require 'strict'
 local from = require 'util/import'
 local class = from 'util/class' : import 'class'
 local method = from 'util/function' : import 'Function'
+
 local midi = require 'midi'
 local list = from 'util/list' : import 'list'
 
@@ -11,9 +12,41 @@ local ticks = 192
 composition.format = 1
 composition.ticks = ticks
 local track = midi.Track()
-track.events:insert(midi.event.NoteBeginEvent(0 * ticks, 0, 72, 100))
-track.events:insert(midi.event.NoteEndEvent(2 * ticks, 0, 72, 100))
-track.events:insert(midi.event.NoteBeginEvent(0 * ticks, 0, 72, 100))
-track.events:insert(midi.event.NoteEndEvent(2 * ticks, 0, 72, 100))
+
+function midi.Track:note(number, length)
+  self.events:insert(midi.event.NoteBeginEvent(0 * ticks, 0, number, 100))
+  self.events:insert(midi.event.NoteEndEvent(length * ticks, 0, 72, 100))
+end
+
+track:note(76, 1)      -- Ma
+track:note(74, 1)      -- ry
+track:note(72, 1)      -- had
+track:note(74, 1)      -- a
+track:note(76, 1)      -- li
+track:note(76, 1)      -- tle
+track:note(76, 2)      -- lamb
+
+track:note(74, 1)      -- li
+track:note(74, 1)      -- tle
+track:note(74, 2)      -- lamb
+
+track:note(76, 1)      -- li
+track:note(79, 1)      -- tle
+track:note(79, 2)      -- lamb
+
+track:note(76, 1)      -- ma
+track:note(74, 1)      -- ry
+track:note(72, 1)      -- had
+track:note(74, 1)      -- a
+track:note(76, 1)      -- li
+track:note(76, 1)      -- tle
+track:note(76, 1)      -- lamb
+track:note(76, 1)      -- its
+track:note(74, 1)      -- fleece
+track:note(74, 1)      -- was
+track:note(76, 1)      -- white
+track:note(74, 1)      -- as
+track:note(72, 16)     -- snow
+
 composition.tracks:insert(track)
-composition:write("blah.mid")
+composition:write("mary.mid")
